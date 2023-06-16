1afa3c7ddcd5557d5c2630f35e1f46cac13b7cd6
TemaTerbi <tematerbi@mail.ru>
2023-06-16 15:01:11 +0300

    Task ready

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld secondBean = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catBeanFirst = (Cat) applicationContext.getBean("cat");
+        Cat catBeanSecond = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean.equals(secondBean));
+        System.out.println(catBeanFirst.equals(catBeanSecond));
+
         System.out.println(bean.getMessage());
     }
 }
